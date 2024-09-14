6a6e50b35f17191c25b1456f5277a951cc518ca5
David Michael II <knowdavidmichael@gmail.com>
2024-09-14 01:38:38 +0100

    add styles and themes with light and dark mode

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,27 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import localFont from "next/font/local";
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
+export const fontSans = localFont({
   variable: "--font-sans",
-});
-
-export const fontMono = FontMono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+  src: [
+    {
+      path: "../public/fonts/NeueMontreal-Light.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/NeueMontreal-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/NeueMontreal-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/NeueMontreal-Bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,79 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
       },
+      fontSize: {
+        xxs: "0.625rem", // 10px
+      },
+      inset: {
+        pad: "var(--pad)",
+        "pad-2x": "var(--pad-2x)",
+        "pad-4x": "var(--pad-4x)",
+      },
+      paddingX: {
+        pad: "var(--pad)",
+        "pad-2x": "var(--pad-2x)",
+        "pad-4x": "var(--pad-4x)",
+      },
+      spacing: {
+        pad: "var(--pad)",
+        "pad-2x": "var(--pad-2x)",
+        "pad-4x": "var(--pad-4x)",
+      }
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+      nextui({
+          defaultTheme: "light",
+          defaultExtendTheme: "light",
+          layout: {},
+          themes: {
+              light: {
+                  colors: {
+                      background: "#FFFFFF",
+                      foreground: "#18181B",
+                      primary: {
+                          DEFAULT: "#FFFFFF",
+                          foreground: "#18181B",
+                          50: "#FAFAFA",
+                          100: "#F4F4F5",
+                          200: "#E4E4E7",
+                          300: "#D4D4D8",
+                          400: "#A1A1AA",
+                          500: "#71717A",
+                          600: "#52525B",
+                          700: "#3F3F46",
+                          800: "#27272A",
+                          900: "#18181B",
+                      }
+                  }
+              },
+              dark: {
+                  colors: {
+                      background: "#18181B",
+                      foreground: "#FFFFFF",
+                      primary: {
+                          DEFAULT: "#18181B",
+                          foreground: "#FFFFFF",
+                          50: "#18181B",
+                          100: "#27272A",
+                          200: "#3F3F46",
+                          300: "#52525B",
+                          400: "#71717A",
+                          500: "#A1A1AA",
+                          600: "#D4D4D8",
+                          700: "#E4E4E7",
+                          800: "#F4F4F5",
+                          900: "#FAFAFA",
+                      }
+                  }
+              },
+          },
+      }),
+  ],
 }
