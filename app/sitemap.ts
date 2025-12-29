632c4e8cee5b3bd8ec364aea0797d07223199f1c
David Michael II <knowdavidmichael@gmail.com>
2025-12-29 12:39:17 +0100

    chore(sitemap): Update URLs to include www subdomain
    Update sitemap URLs to use the 'www' subdomain prefix.
    This change ensures consistency with the canonical domain and
    improves SEO by directing all traffic to the preferred URL.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import type {MetadataRoute} from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://gettoknowdavid.vercel.app',
+            url: 'https://www.gettoknowdavid.vercel.app',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 1,
         },
         {
-            url: 'https://gettoknowdavid.vercel.app/works',
+            url: 'https://www.gettoknowdavid.vercel.app/works',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.9,
         },
         {
-            url: 'https://gettoknowdavid.vercel.app/shots',
+            url: 'https://www.gettoknowdavid.vercel.app/shots',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.5,
